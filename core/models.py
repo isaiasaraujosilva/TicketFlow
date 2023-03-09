7d8f40a2ce03f97b8837b07a46d1f40f8111603a
isaiasaraujo1 <isaias-araujo1@hotmail.com>
2023-03-09 17:32:54 -0300

    create model people

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class People(models.Model):
+
+    cpf = models.CharField(max_length=11)
+    fires_name = models.CharField()
+    last_name = models.CharField()
+    sex = models.CharField()
+    birth_date = models.DateField()
+    phone_number = models.CharField()
+    email = models.CharField()
+
