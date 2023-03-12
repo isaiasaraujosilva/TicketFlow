8620e223c7c17f0d1ca40d9dd4aea45ca767658d
isaiasaraujo1 <isaias-araujo1@hotmail.com>
2023-03-11 21:41:51 -0300

    feat: add column name in panel admin

--- core/admin.py
+++ core/admin.py
@@ -3,20 +3,31 @@ from core.models import People, User,Room, Seat, Ticket, InTheaters, Session
 
 # Register your models here.
 
+
+# admin.site.register(People)
+admin.site.register(User)
+admin.site.register(Room)
+admin.site.register(Seat)
+admin.site.register(Ticket)
+admin.site.register(Session)
+
+
+@admin.register(People)
 class PeopleAdmin (admin.ModelAdmin):
-    admin.site.register(People)
+    list_display = ('cpf','fires_name','last_name','sex','birth_date','phone_number','email')
+    
 
-class UserAdmin(admin.ModelAdmin):
-    admin.site.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     admin.site.register(User)
 
-class RoomAdmin(admin.ModelAdmin):
-    admin.site.register(Room)
+# class RoomAdmin(admin.ModelAdmin):
+#     
 
-class SeatAdmin(admin.ModelAdmin):
-    admin.site.register(Seat)
+# class SeatAdmin(admin.ModelAdmin):
+#     
 
-class TicketAdmin(admin.ModelAdmin):
-    admin.site.register(Ticket)
+# class TicketAdmin(admin.ModelAdmin):
+#     
 
-class SessionAdmin(admin.ModelAdmin):
-    admin.site.register(Session)
+# class SessionAdmin(admin.ModelAdmin):
+#     
