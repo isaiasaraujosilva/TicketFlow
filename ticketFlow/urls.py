0a81270ed87e1fde884dcac3f9bf51ae23aea11b
isaiasaraujo <isaiasaraujopi@gmail.com>
2023-12-17 18:56:32 -0300

    feat: add swagger

--- ticketFlow/urls.py
+++ ticketFlow/urls.py
@@ -17,6 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from core.views import PeopleViewSet, UserViewSet, RoomViewSet, SeatViewSet, SessionViewSet, TickectViewSet
 from rest_framework import routers
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+# from rest_framework_swagger.views import get_swagger_view
+
 
 router = routers.DefaultRouter()
 router.register('People',PeopleViewSet)
@@ -26,8 +32,24 @@ router.register('Seat',SeatViewSet)
 router.register('Session',SessionViewSet)
 router.register('Ticket',TickectViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="contact@yourapp.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+# schema_view = get_swagger_view(title='API Documentation')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include(router.urls)),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    
 ]
